85d5b6f179bda746ab08bf922858f8cbc88584ae
Maria Leidinice da Silva <leidinicesilva@gmail.com>
2025-09-24 15:46:11 +0200

    update scripts

--- plot_tf-cprcm_fig_maps_ecyclone.py
+++ plot_tf-cprcm_fig_maps_ecyclone.py
@@ -16,13 +16,13 @@ from matplotlib.patches import Circle
 from scipy.interpolate import griddata
 from cartopy.mpl.ticker import LongitudeFormatter, LatitudeFormatter
 
-path = '/afs/ictp.it/home/m/mda_silv/Downloads'
+path = '/leonardo/home/userexternal/mdasilva/leonardo_work/TRACK-CYCLONE/CPRCM-TF'
 font_size = 10
-
+day=24
 
 def import_data(param):
 	
-	arq = '{0}/{1}_Mar26_lonlat.nc'.format(path, param)
+	arq = '{0}/data/{1}_Mar{2}_lonlat.nc'.format(path, param, day)
 	data = netCDF4.Dataset(arq)
 	var  = data.variables[param][:]    
 	lat  = data.variables['lat'][:]
@@ -30,10 +30,13 @@ def import_data(param):
 	mean = var[:, :, :]
 	
 	if param == 'psl':
+		#mean = var[-1, :, :] / 100.0
 		mean = np.nanmean(var[:, :, :], axis=0) / 100.0
 	elif param == 'pr':
+		#mean = var[-1, :, :] * 3600
 		mean = np.nansum(var[:, :, :], axis=0) * 3600
 	else:
+		#mean = var[-1, :, :]
 		mean = np.nanmean(var[:, :, :], axis=0)
 						
 	return lat, lon, mean
@@ -56,16 +59,16 @@ def configure_subplot(ax):
 	ax.set_xlabel('Longitude', fontsize=font_size, fontweight='bold')
 	ax.set_ylabel('Latitude',  fontsize=font_size, fontweight='bold')
 	ax.grid(c='k', ls='--', alpha=0.5)
-	ax.add_feature(cfeat.BORDERS, linewidth=1, color='gray')
-	ax.coastlines(linewidth=1, color='gray')	
+	ax.add_feature(cfeat.BORDERS, linewidth=2, color='gray')
+	ax.coastlines(linewidth=2, color='gray')	
 
 
 # Plot figure
 fig, ax = plt.subplots(subplot_kw={"projection": ccrs.PlateCarree()})
 step = 4
-levels_pr = np.arange(0, 31, 1)
-levels_psl = np.arange(1010, 1020.5, 0.5)
-levels_ta925 = np.arange(288, 291, 0.5)
+levels_pr = np.arange(0, 31.5, 0.5)
+levels_psl = np.arange(990, 1020.5, 0.5)
+levels_ta925 = np.arange(286, 301, 1)
 
 cf = ax.contourf(lon, lat, pr, levels=levels_pr, cmap='Blues', extend='max')
 cb = plt.colorbar(cf)
@@ -83,8 +86,8 @@ ax.clabel(ct2, inline=1, fontsize=font_size-3)
 configure_subplot(ax)
 
 # Save figura
-path_out = '{0}'.format(path)
-name_out = 'pyplt_maps_Hurricane_Catarina_26Mar2004.png'
+path_out = '{0}/figs/'.format(path)
+name_out = 'pyplt_maps_Hurricane_Catarina_{0}Mar2004.png'.format(day)
 plt.savefig(os.path.join(path_out, name_out), dpi=400, bbox_inches='tight')
 plt.show()
 exit()
