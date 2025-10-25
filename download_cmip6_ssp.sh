8aa26c7845aa171120542b136be4751c87eed287
Leidinice <leidinicesilva@gmail.com>
2025-10-25 18:31:59 +0200

    update scripts

--- download_cmip6_ssp.sh
+++ download_cmip6_ssp.sh
@@ -7,27 +7,33 @@
 #SBATCH --mail-type=FAIL,END
 #SBATCH --mail-user=mda_silv@ictp.it
 
-mdl="TaiESM1"
+mdl="MIROC6"
 exp="ssp585"
 
-if [ ${mdl} == 'ACCESS-ESM1-5' ]; then # ACCESS-ESM1-5
+if [ ${mdl} == 'ACCESS-ESM1-5' ]; then 
 	mdl_family="CSIRO"
 	member="r1i1p1f1"
 	type="gn"
 	version="v20210318"
 	declare -a YEARS=("20150101-20641231" "20650101-21001231")
+elif [ ${mdl} == 'EC-Earth3' ]; then
+	mdl_family="EC-Earth-Consortium"
+	member="r1i1p1f1"
+	type="gr"
+	version="v20200310" 
+	declare -a YEARS=($(seq 2015 2100))
+elif [ ${mdl} == 'EC-Earth3-Veg-LR' ]; then
+	mdl_family="EC-Earth-Consortium"
+	member="r1i1p1f1"
+	type="gr"
+	version="v20201201" 
+	declare -a YEARS=($(seq 2015 2100))
 elif [ ${mdl} == 'GFDL-ESM4' ]; then
 	mdl_family="NOAA-GFDL"
 	member="r1i1p1f1"
 	type="gr1"
 	version="v20180701"
 	declare -a YEARS=("20150101-20341231" "203650101-20741231" "20750101-20941231" "20950101-21001231")
-elif [ ${mdl} == 'INM-CM5-0' ]; then
-	mdl_family="INM"
-	member="r1i1p1f1"
-	type="gr1"
-	version="v20190619"
-	declare -a YEARS=("20150101-20641231" "20650101-21001231")
 elif [ ${mdl} == 'HadGEM3-GC31-MM' ]; then
 	mdl_family="MOHC"
 	exp="ssp585"
@@ -35,6 +41,18 @@ elif [ ${mdl} == 'HadGEM3-GC31-MM' ]; then
 	type="gn"
 	version="v20200515"
 	declare -a YEARS=("20150101-20191230" "20200101-20241230" "20250101-20291230" "20300101-20341230" "20350101-20391230" "20400101-20441230" "20450101-20491230" "20500101-20541230" "20550101-20591230" "20600101-20641230" "20650101-20691230" "20700101-20741230" "20750101-20791230" "20800101-20841230" "20850101-20891230" "20900101-20941230" "20950101-20991230" "21000101-21001230")
+elif [ ${mdl} == 'INM-CM5-0' ]; then
+	mdl_family="INM"
+	member="r1i1p1f1"
+	type="gr1"
+	version="v20190619"
+	declare -a YEARS=("20150101-20641231" "20650101-21001231")
+elif [ ${mdl} == 'MIROC6' ]; then
+	mdl_family="MIROC"
+	member="r1i1p1f1"
+	type="gn"
+	version="v20191016"
+	declare -a YEARS=("20150101-20241231" "20250101-20341231" "20350101-20441231" "20450101-20541231" "20550101-20641231" "20650101-20741231" "20750101-20841231" "20850101-20941231" "20950101-21001231")
 elif [ ${mdl} == 'MPI-ESM1-2-HR' ]; then
 	mdl_family="DKRZ"
 	member="r1i1p1f1"
@@ -68,19 +86,23 @@ else
 	declare -a YEARS=("20150101-20241231" "20250101-20341231" "20350101-20441231" "20450101-20541231" "20550101-20641231" "20650101-20741231" "20750101-20841231" "20850101-20941231" "20950101-21001231")
 fi
 
-var_list="pr tasmax tasmin"
+var_list="tasmin"
 
 echo "Starting download"
 for var in ${var_list[@]}; do
 
-	base_url="https://data.ceda.ac.uk/badc/cmip6/data/CMIP6/ScenarioMIP/${mdl_family}/${mdl}/${exp}/${member}/day/${var}/${type}/${version}"
-	dir="/home/mda_silv/clima-archive2-b/CMIP6/${mdl}"
-	
+	base_url="https://esgf.ceda.ac.uk/thredds/fileServer/esg_cmip6/CMIP6/ScenarioMIP/${mdl_family}/${mdl}/${exp}/${member}/day/${var}/${type}/${version}"
+	dir="/home/mda_silv/clima-archive2-b/ETCCDI_paper/CMIP6/${mdl}"
+
 	for year in "${YEARS[@]}"; do
 
-		filename="${var}_day_${mdl}_${exp}_${member}_${type}_${year}.nc"
-		url="${base_url}/${filename}"
+		if [ ${mdl} == 'EC-Earth3' ] || [ ${mdl} == 'EC-Earth3-Veg-LR' ]; then
+			filename="${var}_day_${mdl}_${exp}_${member}_${type}_${year}0101-${year}1231.nc"
+		else
+			filename="${var}_day_${mdl}_${exp}_${member}_${type}_${year}.nc"
+		fi
 		
+		url="${base_url}/${filename}"
 		file_dir="/home/mda_silv/clima-archive2-b/CMIP6/${mdl}/${filename}"
 
 		if [ -f "$file_dir" ]; then
